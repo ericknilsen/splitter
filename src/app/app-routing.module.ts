d77d841c21f7169c4be62b14ee06a0fca6b2071c
Erick Souza <ericknilsen@gmail.com>
2021-01-30 21:59:34 -0800

    Adding routing config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddExpenseComponent } from './add-expense/add-expense.component';
+import { ListExpensesComponent } from './list-expenses/list-expenses.component';
+import { ReportComponent } from './report/report.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'add-expenses', component: AddExpenseComponent },
+  { path: 'list-expenses', component: ListExpensesComponent },
+  { path: 'report-expenses', component: ReportComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
