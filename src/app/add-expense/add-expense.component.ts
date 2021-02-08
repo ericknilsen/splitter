9613cf30f676f6bdbffb0f95228781c23720ce2e
Erick Souza <ericknilsen@gmail.com>
2021-02-07 22:23:43 -0800

    adding proportion field

--- src/app/add-expense/add-expense.component.ts
+++ src/app/add-expense/add-expense.component.ts
@@ -14,13 +14,24 @@ import {
 export class AddExpenseComponent implements OnInit {
 
   form: FormGroup;
-  categories = ['Grocery', 'Restaurant', 'Service', 'Recreation'];
+  defaultProportion = 37;
+  halfProportion = 50;
+
+  categories = new Map([['Grocery', this.defaultProportion],
+                      ['Housing', this.defaultProportion],
+                      ['Internet', this.defaultProportion],
+                      ['Hydro', this.defaultProportion],
+                      ['Gas', this.halfProportion],
+                      ['Car Service', this.halfProportion],
+                      ['Recreation', this.halfProportion],
+                      ['Restaurant', this.halfProportion]]);
 
   constructor(private fb: FormBuilder) {
     this.form = fb.group({
       'description': new FormControl('', Validators.required),
-      'date': new FormControl('', Validators.required),
+      'date': new FormControl(this.currentDate(), Validators.required),
       'amount': new FormControl('', Validators.required),
+      'proportion': new FormControl('', Validators.required),
       'category': new FormControl('', Validators.required)
     });
   }
@@ -33,5 +44,18 @@ export class AddExpenseComponent implements OnInit {
   }
 
   get f() { return this.form.controls; }
+
+  currentDate() {
+    const today = new Date();
+    const dd = String(today.getDate()).padStart(2, '0');
+    const mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    const yyyy = today.getFullYear();
+
+    return mm + '/' + dd + '/' + yyyy; 
+  }
+
+  selectCategory(key: any) {
+    this.form.patchValue({'proportion': this.categories.get(key)+'%'});
+  }
 
 }
