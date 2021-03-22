da5a32db25b2bd415a8222ec9013bb0f861da719
Erick Souza <ericknilsen@gmail.com>
2021-03-21 19:23:46 -0700

    Approve expenses and payment

--- src/app/utils/constants.ts
+++ src/app/utils/constants.ts
@@ -1,9 +1,9 @@
 
 //prod
-//export const ENDPOINT = 'https://splitter-api.herokuapp.com';
+export const ENDPOINT = 'https://splitter-api.herokuapp.com';
 
 //dev
-export const ENDPOINT = 'http://localhost:3000';
+//export const ENDPOINT = 'http://localhost:3000';
 
 export const GOOGLE_OAUTH_CLIENT_ID = '949636290315-dam2hednkffafgl1g5abm641soele312.apps.googleusercontent.com';
 
