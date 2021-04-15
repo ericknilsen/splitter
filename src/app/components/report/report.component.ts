89dd73087694790f3ead29c9161ea033c8adbccb
Erick Souza <ericknilsen@gmail.com>
2021-04-14 21:28:59 -0700

    Reports

--- src/app/components/report/report.component.ts
+++ src/app/components/report/report.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ChartType } from 'chart.js';
 import { MultiDataSet, Label } from 'ng2-charts';
+import { Util } from 'src/app/common/util';
+import { Expense } from 'src/app/models/expense.model';
+import { ExpensesService } from 'src/app/services/expenses.service';
 
 @Component({
   selector: 'app-report',
@@ -8,16 +11,48 @@ import { MultiDataSet, Label } from 'ng2-charts';
   styleUrls: ['./report.component.css']
 })
 export class ReportComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-  doughnutChartLabels: Label[] = ['BMW', 'Ford', 'Tesla'];
+  doughnutChartLabels: Label[] = [];
   doughnutChartData: MultiDataSet = [
-    [55, 25, 20]
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
   ];
   doughnutChartType: ChartType = 'doughnut';
+
+  user: any;
+  expenses: Expense[] = [];
+
+  constructor(private expensesService: ExpensesService) { }
+
+  ngOnInit(): void {
+    this.user = Util.getCurrentUser();
+    
+    this.listExpenses();
+  }
+
+  private listExpenses() {
+    this.expensesService.listExpensesByUser(this.user.email).subscribe(expenses => {
+      this.expenses = expenses;
+      this.buildTotalExpensesByCategoryChart();
+    });
+  }
+
+  buildTotalExpensesByCategoryChart() {
+    let aggregatedExpensesByCategory = this.expenses
+      .map(e => {
+        return { category: e.category, amount: e.amount }
+      })
+      .reduce((result: any[], current: any) => {
+        const found = result.filter(e => e.category === current.category)
+        if (found.length === 0) {
+          result.push(current);
+        } else {
+          found[0].amount = parseFloat(found[0].amount) + parseFloat(current.amount);
+        }
+        return result;
+      }, [])
+
+    this.doughnutChartLabels = aggregatedExpensesByCategory.map(a => a.category);
+    this.doughnutChartData = [aggregatedExpensesByCategory.map(a => a.amount.toFixed(2))];
+  }
+
 
 }
